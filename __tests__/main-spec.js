95958c7ddfbca1694a2b679bdb9f7f91ef25ce48
hardy <hard.t.z.wei@oocl.com>
2020-07-21 17:13:50 +0800

    add files

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -4,9 +4,24 @@ describe("create multiply table", () => {
   it("should return multiply table when given start is 2 and end is 4", () => {
     const start = 2;
     const end = 4;
-
     const multiplyTable = main.createMultiplyTable(start, end);
-
     expect(multiplyTable).toBe("2*2=4\n2*3=6\t3*3=9\n2*4=8\t3*4=12\t4*4=16");
   });
+
+  it("should return null when given start is 90 and end is 40",()=>{
+    const start = 90;
+    const end = 40;
+    const multiplyTable = main.createMultiplyTable(start, end);
+    expect(multiplyTable).toBe(null);
+  });
+  
+  it("should return null when given start is 0 and end is 40",()=>{
+    const start = 0;
+    const end = 40;
+    const multiplyTable = main.createMultiplyTable(start, end);
+    expect(multiplyTable).toBe(null);
+  });
+
 });
+
+ 
--- main.js
+++ main.js
@@ -1,5 +1,57 @@
 function createMultiplyTable(start, end) {
+    if(isValid(start,end)) {
+      return generateAllLines(start,end);
+    }
   return null;
+}
+
+function isValid(start,end){
+  if(checkStartAndEndNumber(start,end) && checkStartAndEndNumberRange(start,end)){
+      return true;
+  }
+  return false;
+}
+
+function checkStartAndEndNumber(start,end) {
+  return start < end;
+}
+
+function checkStartAndEndNumberRange(start,end) {
+  return (checkNumberRange(start)&&checkNumberRange(end));
+}
+
+function checkNumberRange(number) {
+  return  (  1 < number && number < 1000);
+}
+
+function generateAllLines(start,end) {
+  let i;
+  let ans = "";
+  for(i = start; i <= end; i++) {
+      ans += generateLine(start,i);
+      if(i != end) {
+        ans += "\n";
+      }
+  }
+  return ans;
+}
+
+function generateLine(lineStart,lineEnd) {
+  let i;
+  let lineAns = "";  
+  for(i = lineStart ; i <= lineEnd; i++) {
+      lineAns += generateExpression(i,lineEnd)  ;
+      if(i != lineEnd) {
+        lineAns += "\t";
+      }
+  }
+  return lineAns;
+}
+
+function generateExpression(multiplier,multicand) {
+    let expression;
+    expression = multiplier + "*" + multicand + "=" + (multiplier*multicand);
+    return expression;
 }
 
 module.exports = {
